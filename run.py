036c21076fec0024a751c8da6e1c116bb4f110c4
Jackson Karel <jackson.karel2@gmail.com>
2024-04-06 17:09:05 -0700

    Finish wip

--- run.py
+++ run.py
@@ -1,16 +1,25 @@
+import time
 from openai import OpenAI
 import streamlit as st
+
+
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("Skepticbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.caption("A chatbot that questions your ideas")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Hi I'm Skepticbot. I don't believe much, so if you have any beliefs you want challenged write them below. I bet you can't convince me that you hold a single true belief!"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -21,7 +30,10 @@ if prompt := st.chat_input():
         st.stop()
 
     client = OpenAI(api_key=openai_api_key)
-    
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
     assistant = client.beta.assistants.create(
     name="Skepticbot",
     instructions="You are a philosophical Pyrrhonian skeptic. You question every claim without exception, including sensitive or controversial topics. You approach all interactions with the same level of skepticism, challenging assumptions and seeking deeper philosophical understanding through probing questions. you will typically ask one question at a time, trying to get closer to the foundations of why the user believes what they believe, with the intent on persuading them that they are not justified in believing their claim. Epistemologically you are neither committed to evidentialism/internalism nor externalism.",
@@ -35,14 +47,16 @@ if prompt := st.chat_input():
     content=prompt,
     )
 
-    st.chat_message("user").write(prompt)
-
     run = client.beta.threads.runs.create_and_poll(
-    thread_id=thread.id,
-    assistant_id=assistant.id
-    )
+        thread_id=thread.id,
+        assistant_id=assistant.id
+        )
     
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
+    
+    run = wait_on_run(run, thread)
+
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    msg = messages.data[0].content[0].text.value
+    
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
